d7f73b7ea64e83f10897d26f39432d87d65cd63a
Haileigh <hl863882@dal.ca>
2023-12-02 03:30:33 -0400

    Item discovery messages working

--- Lost-Lullaby/Assets/Scripts/House/HouseItems.cs
+++ Lost-Lullaby/Assets/Scripts/House/HouseItems.cs
@@ -1,20 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HouseItems : MonoBehaviour
 {
     [SerializeField] GameObject item;
-    [SerializeField] AudioClip clip;
+    //[SerializeField] AudioClip clip;
+    [SerializeField] GameObject itemInfo;
+    bool shown = false;
 
     void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.tag == "Player")
+        if (collider.tag == "Player" && shown == false) //only display if player hasnt seen it already
         {
-            AudioSource.PlayClipAtPoint(clip, transform.position);
-            //... We want to remove the entire object from scene
-            Debug.Log("Destroyed item");
-            Destroy(item);
+            //AudioSource.PlayClipAtPoint(clip, transform.position);
+            Debug.Log("Contact with item");
+            StartCoroutine(DisplayMessage(itemInfo));
+
         }
+    }
+
+    public IEnumerator DisplayMessage(GameObject itemInfo)
+    {
+        itemInfo.SetActive(true);
+        yield return new WaitForSeconds(4);     //show for 4 seconds
+        itemInfo.SetActive(false);
     }
 }
