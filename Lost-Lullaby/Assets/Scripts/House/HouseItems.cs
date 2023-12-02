0dfd7986e963b7284aad0b0e942b2a11dfd90ac3
Haileigh <hl863882@dal.ca>
2023-12-02 15:21:09 -0400

    Item Counter working

--- Lost-Lullaby/Assets/Scripts/House/HouseItems.cs
+++ Lost-Lullaby/Assets/Scripts/House/HouseItems.cs
@@ -9,22 +9,37 @@ public class HouseItems : MonoBehaviour
     //[SerializeField] AudioClip clip;
     [SerializeField] GameObject itemInfo;
     bool shown = false;
+    static int itemCount = 0;
+    [SerializeField] Text itemCounter; 
+    [SerializeField] Text itemCounterShadow;
+    static bool showingMessage = false;
 
-    void OnTriggerEnter2D(Collider2D collider)
+    private void Start()
+    {
+        itemCounter.text = itemCount.ToString();
+    }
+
+    IEnumerator OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.tag == "Player" && shown == false) //only display if player hasnt seen it already
         {
             //AudioSource.PlayClipAtPoint(clip, transform.position);
             Debug.Log("Contact with item");
+            shown = true;
+            yield return new WaitUntil(() => !showingMessage);
             StartCoroutine(DisplayMessage(itemInfo));
-
         }
     }
 
     public IEnumerator DisplayMessage(GameObject itemInfo)
     {
+        showingMessage = true;
+        itemCount++;
+        itemCounter.text = itemCount.ToString();
+        itemCounterShadow.text = itemCount.ToString();
         itemInfo.SetActive(true);
         yield return new WaitForSeconds(4);     //show for 4 seconds
         itemInfo.SetActive(false);
+        showingMessage = false;
     }
 }
