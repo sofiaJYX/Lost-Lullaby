6d657f9b4d004487090e6ed0bc6f401f256c2eea
ewert <mewert@dal.ca>
2023-12-03 20:42:19 -0400

    Implemented camera switch on pressing c between fawn and player

--- Lost-Lullaby/Assets/Scripts/Camera Scripts/CameraFollow.cs
+++ Lost-Lullaby/Assets/Scripts/Camera Scripts/CameraFollow.cs
@@ -4,18 +4,39 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target; // The player's Transform
+    public Transform player; // The player's Transform
+    public Transform fawn; // The fawn's transform
+
+    public Transform target;
+
+    private bool fawnCam = false;
 
     public float smoothSpeed = 0.125f; // The smoothness of camera movement
 
     public Vector3 offset; // The offset from the player
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            fawnCam = !fawnCam;
+        }
+    }
+
     void FixedUpdate()
     {
-        if (target == null)
+        if (player == null || fawn == null)
         {
             Debug.LogError("Camera target not set!");
             return;
+        }
+
+        if (fawnCam)
+        {
+            target = fawn;
+        } else
+        {
+            target = player;
         }
 
         Vector3 desiredPosition = target.position + offset;
